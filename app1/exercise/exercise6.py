0e35645a420fbddfc830375d8c5ae43fdae503d2
Harun Ibrisevic <harun.car@gmail.com>
2023-03-12 12:47:00 +0100

    Revert "Initial commit"
    This reverts commit dbd3404127fae6077e2a4e22cd9bd24dcce9219b.

--- app1/exercise/exercise6.py
+++
@@ -1,5 +0,0 @@
-from exercise6_function import state_of_water
-
-temperature=int(input("Please enter water temperature: "))
-
-print(state_of_water(temperature))
--- app1/exercise/exercise7.py
+++
@@ -1,6 +0,0 @@
-import random
-
-number1=int(input("Please enter first number: "))
-number2=int(input("Please enter second number: "))
-
-print(random.randint(number1,number2))

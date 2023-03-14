c20477bbaf92d9628feb62325e3001f3dd0bb376
Harun Ibrisevic <harun.car@gmail.com>
2023-03-14 15:18:15 +0100

    TODO APP-implement complete and exit button 14.03.2023

--- app1/gui.py
+++ app1/gui.py
@@ -9,8 +9,12 @@ list_box=sg.Listbox(values=functions.get_todos(),
                     enable_events=True,
                     size=[45,10])
 edit_button=sg.Button("Edit")
-
-window=sg.Window('My To-Do App', layout=[[label],[input_box,add_button],[list_box,edit_button ]],
+complete_button = sg.Button("Complete")
+exit_button=sg.Button("Exit")
+window=sg.Window('My To-Do App', layout=[[label],
+                                         [input_box,add_button],
+                                         [list_box,edit_button,complete_button ],
+                                         [exit_button]],
                  font=('Helvetica',12))
 
 while True:
@@ -35,11 +39,20 @@ while True:
             functions.write_todos(todos)
             #update kod novim unosom
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             #update mjesto za unos
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
-            break
+            exit()
 
 
 window.close()

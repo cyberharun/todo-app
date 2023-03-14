2b6a49a045079a36f58547f77c00563f178244a0
Harun Ibrisevic <harun.car@gmail.com>
2023-03-14 15:54:59 +0100

    TODO APP-adding images to labels and edit it 14.03.2023

--- app1/gui.py
+++ app1/gui.py
@@ -6,13 +6,15 @@ sg.theme("LightBrown4")
 clock=sg.Text('',key="clock")
 label = sg.Text("Type in a to-do")
 input_box=sg.InputText(tooltip="Enter todo",key="todo")
-add_button=sg.Button("Add",size=10)
+add_button=sg.Button(size=2, image_source="add.png",mouseover_colors="LightBlue2",
+                     tooltip="Add todo",key="Add")
 list_box=sg.Listbox(values=functions.get_todos(),
                     key = 'todos',
                     enable_events=True,
                     size=[45,10])
 edit_button=sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=20, image_source="complete.png", mouseover_colors="LightGreen",
+                            tooltip="Complete",key="Complete")
 exit_button=sg.Button("Exit")
 window=sg.Window('My To-Do App', layout=[[clock],
                                          [label],
